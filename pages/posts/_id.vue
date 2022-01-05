0a9d050d62c2db3d44f2c9272e81d827db3eca4c
Mads Agergaard <maj@novicell.dk>
2022-01-05 12:12:03 +0100

    Dynamic posts

---
+++ pages/posts/_id.vue
@@ -0,0 +1,69 @@
+
+<template>
+  <div class="container">
+    <article>
+      <h1 class="title">{{post.title}}</h1>
+      <p>{{post.content}}</p>
+    </article>
+  </div>
+</template>
+
+<script>
+  export default {
+
+    data () {
+
+      return{
+
+        id: this.$route.params.id,
+
+        posts: [
+
+          {
+           id: 'balut',
+           title: 'What is Balut?',
+           content: 'If someone placed balut on your plate, you might think they were serving you a hardboiled egg. That is, until you cracked it open and a fully intact duck embryo spilled out. Balut, considered a delicacy in many Asian countries, is produced when fertilized duck eggs are placed in warm sunlight. After about eight days, the eggs are held up to the light and checked to ensure that the budding embryo is ready. Then, the eggs are cooked and served with a dash of salt and a few squirts of lemon juice.'
+          },
+          {
+           id: 'serum',
+           title: 'Lorem ipsum idk',
+           content: 'If someone placed balut on your plate, you might think they were serving you a hardboiled egg. That is, until you cracked it open and a fully intact duck embryo spilled out. Balut, considered a delicacy in many Asian countries, is produced when fertilized duck eggs are placed in warm sunlight. After about eight days, the eggs are held up to the light and checked to ensure that the budding embryo is ready. Then, the eggs are cooked and served with a dash of salt and a few squirts of lemon juice.'
+          },
+          {
+           id: 'papirkurv',
+           title: 'Vue school',
+           content: 'If someone placed balut on your plate, you might think they were serving you a hardboiled egg. That is, until you cracked it open and a fully intact duck embryo spilled out. Balut, considered a delicacy in many Asian countries, is produced when fertilized duck eggs are placed in warm sunlight. After about eight days, the eggs are held up to the light and checked to ensure that the budding embryo is ready. Then, the eggs are cooked and served with a dash of salt and a few squirts of lemon juice.'
+          },
+        ]
+      }
+    },
+
+    computed: {
+      post () {
+        return this.posts.find(post => post.id === this.id)
+      }
+    }
+
+
+  }
+
+</script>
+
+<style scoped>
+  .container {
+    display: flex;
+    justify-content: space-between;
+    line-height: 1.5;
+  }
+  article * {
+    margin-bottom: 1rem;
+  }
+  aside {
+    min-width: 280px;
+    max-width: 280px;
+    padding-left: 2rem;
+  }
+  .title {
+    font-size: 2rem;
+  }
+</style>
